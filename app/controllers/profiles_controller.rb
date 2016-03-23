b1e5f21a22c79b83d51b8ad98fc5bbad426459b3
Saranya Thirumeni <thirumenisaranya@gmail.com>
2016-03-23 00:21:19 +0000

    Added before filters to check correct users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
         #form where user can fill out their own profile.
         @user = User.find(params[:user_id])
@@ -14,12 +17,27 @@ class ProfilesController < ApplicationController
         else
             render action: :new
         end
-        
     end
     
     def edit
        @user = User.find(params[:user_id])
        @profile = @user.profile
+    end
+    
+    def update
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile Updated"
+            redirect_to user_path(params[:user_id])
+        else
+            render action: :edit
+        end
+    end
+    
+    def only_current_user
+        @user = User.find(params[:user_id])
+        redirect_to(root_url) unless @user == current_user
     end
     
     # white listing the parameters
